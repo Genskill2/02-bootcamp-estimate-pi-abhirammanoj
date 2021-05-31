4467771c80c4f3f92d6ae770db6fcfa416152d30
abhirammanoj <abhirammanojabhayam@gmail.com>
2021-05-31 19:00:12 +0530

    2 functions added

--- estimate.py
+++ estimate.py
@@ -1,5 +1,30 @@
 import math
 import unittest
+import random
+
+def wallis(n):
+    pi = 0.0   
+    for i in range(1, n+1):
+        x = 4 * (i ** 2)
+        y = x - 1
+        z = float(x) / float(y)
+        if i == 1:
+            pi = z
+        else:
+            pi *= z
+    pi *= 2
+    return pi
+
+def monte_carlo(i):
+    c = 0
+    for n in range(1, i+1):
+        x = random.random()
+        y = random.random()
+        d = x*x + y*y
+        if d <= 1.000000000:
+            c +=1
+    ans = 4*c/i
+    return ans
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
